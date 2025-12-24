659f772238d5acba68672d52036d90604d9af142
git__king <193081202@qq.com>
2025-12-24 22:17:03 +0800

    test

--- 2025/2025_12/12_24/test01/test01/test.c
+++ 2025/2025_12/12_24/test01/test01/test.c
@@ -2,26 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+//Test1: 不使用<string.h>头文件
 void Test_1()
-{
-	char str[80] = { 0 };
-	char substr[80] = { 0 };
-	fgets(str, 80, stdin);
-	fgets(substr, 80, stdin);
-	int len = strlen(substr) - 1;
-	char* prev, * curr;
-	prev = curr = str;
-	while (*curr)
-	{
-		if (strncmp(curr, substr, len) == 0)
-			curr += len;
-		else
-			*(prev++) = *(curr++);
-	}
-	*prev = '\0';
-	printf("%s\n", str);
-}
-void Test_2()
 {
 	char str[80] = { 0 };
 	char substr[80] = { 0 };
@@ -54,7 +36,47 @@ void Test_2()
 	*prev = '\0';
 	printf("%s\n", str);
 }
+
+//Test2: 使用strncmp函数
+void Test_2()
+{
+	char str[80] = { 0 };
+	char substr[80] = { 0 };
+	fgets(str, 80, stdin);
+	fgets(substr, 80, stdin);
+	int len = strlen(substr) - 1;
+	char* prev, * curr;
+	prev = curr = str;
+	while (*curr)
+	{
+		if (strncmp(curr, substr, len) == 0)
+			curr += len;
+		else
+			*(prev++) = *(curr++);
+	}
+	*prev = '\0';
+	printf("%s\n", str);
+}
+
+//Test3: 使用strstr, memcpy函数
 void Test_3()
+{
+	char str[80] = { 0 };
+	char substr[80] = { 0 };
+	gets(str, 80, stdin);//gets非常不安全
+	gets(substr, 80, stdin);
+
+	char* pos = NULL;
+	while (pos = strstr(str, substr))
+	{
+		memmove(pos, pos + strlen(substr), str + strlen(str) - pos);
+	}
+
+	printf("%s\n", str);
+}
+
+//Test3: 使用strstr, memcpy函数, 使用更安全的fget函数
+void Test_4()
 {
 	char str[80] = { 0 };
 	char substr[80] = { 0 };
@@ -62,22 +84,6 @@ void Test_3()
 	fgets(substr, 80, stdin);
 	str[strcspn(str, "\n")] = '\0';
 	substr[strcspn(substr, "\n")] = '\0';
-
-	char* pos = NULL;
-	while (pos = strstr(str, substr))
-	{
-		memmove(pos, pos + strlen(substr), str + strlen(str) - pos);
-	}
-	
-	printf("%s\n", str);
-}
-
-void Test_4()
-{
-	char str[80] = { 0 };
-	char substr[80] = { 0 };
-	gets(str, 80, stdin);
-	gets(substr, 80, stdin);
 
 	char* pos = NULL;
 	while (pos = strstr(str, substr))
